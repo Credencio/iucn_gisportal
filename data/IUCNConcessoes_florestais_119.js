2e69e8201a8ce81cc52898489ac50ddbc9244b86
Credencio <33226477+Credencio@users.noreply.github.com>
2025-09-02 14:29:22 +0200

    Add files via upload

---
+++ data/IUCNConcessoes_florestais_119.js
@@ -0,0 +1,1 @@
+var json_IUCNConcessoes_florestais_119 = {"type":"FeatureCollection","name":"IUCNConcessoes_florestais_119","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
